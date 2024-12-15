be305601866d2e60991475970c430af62534af2f
Avanthikacoding <aviedayilliam@gmail.com>
2024-12-15 12:21:10 +0000

    Add files via upload

---
+++ Activity 1.py
@@ -0,0 +1,20 @@
+a = 20
+b = 20
+c = 20
+if a and b and c:
+    print("All of the values are of bolean values and are true!")
+else: 
+    print("They are not of bolean value! Try again!")
+
+a = -10
+b = -1
+c = -2
+if a > 0 or b > 0 :
+    print("Either of the values are true! Hooray!")
+else: 
+    print("This is false!")
+
+if b > 0 or c > 0 :
+    print("Either of the values are true! Fantastic 😊")
+else:
+    print("This is wrong! Sorry 😔 Please try again!")
---
+++ Activity 2.py
@@ -0,0 +1,27 @@
+a = 5
+y = 10
+l = 10
+
+print(a != y)
+print(y != l)
+
+
+t = "Avanthika"
+i = "word"
+
+if t !=i :
+    print(t , "and", i, "are different!")
+
+m = 25
+j = 50
+
+if (m == 3) != (j == 5) :
+    print("Yay!Sucess!😜")
+
+h = 40
+r = 35
+
+if r%2 != 0:
+    print("H is not an even number! Try again 😔")
+else:
+    print("Yess!It is an even number! Good job 🥰")
